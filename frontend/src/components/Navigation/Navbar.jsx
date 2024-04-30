f9363ff6bcf5e800aff8b666688063fe08d7b3dc
3mpty <aurelien.corre@outlook.fr>
2024-04-30 12:59:34 +0200

    Created Navbar

--- frontend/src/components/Navigation/Navbar.jsx
+++ frontend/src/components/Navigation/Navbar.jsx
@@ -2,15 +2,26 @@ import { Link } from "react-router-dom";
 import styles from "./navbar.module.css";
 
 import logo from "../../../../designs/img/argentBankLogo.png";
+import profilIcon from "../../../../designs/img/profil-icon.svg";
 
 const Navbar = () => {
     return (
         <nav className={styles.nav}>
-            <img src={logo} alt="Argen Bank's logo" />
+            <Link to={"/"} target="_top">
+                <img
+                    className={styles.logo}
+                    src={logo}
+                    alt="Argen Bank's logo"
+                />
+            </Link>
             <ul>
                 <li>
                     <Link to={"/sign-in"} target="_top">
-                        <img src="" alt="Profil Icon" />
+                        <img
+                            className={styles.profil}
+                            src={profilIcon}
+                            alt="Profil Icon"
+                        />
                         <p>Sign In</p>
                     </Link>
                 </li>
